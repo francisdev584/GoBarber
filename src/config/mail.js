38503509d06222ff1256fc6e83c038516082fc01
Francinildo Rodrigues <francis584@gmail.com>
2019-07-16 23:24:44 -0300

    config dotenv

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
-    user: 'a3b02d60a86133',
-    pass: '6e6b44b120484f',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
